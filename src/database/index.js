299eade7384f86ea6eb7854308f9a883613f01bc
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-08-02 12:03:17 -0300

    Inicializando a tabela de generos

--- src/database/index.js
+++ src/database/index.js
@@ -4,17 +4,16 @@ const dbConfig = require('../config/database');
 const Movie = require('../models/Movie');
 const Actor = require('../models/Actor');
 
-// const Address = require('../models/Address');
+const Gender = require('../models/Gender');
 
 const connection = new Sequelize(dbConfig);
 
 Movie.init(connection);
 Actor.init(connection);
-// Tech.init(connection);
+Gender.init(connection);
 
 Movie.associate(connection.models);
 Actor.associate(connection.models);
-
-// Address.associate(connection.models);
+Gender.associate(connection.models);
 
 module.exports = connection;
